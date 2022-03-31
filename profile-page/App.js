f18cfcbf35285e24c0910233754fcc4c78494a1a
Asadullah Khan <asadullahk15@gmail.com>
2022-03-31 14:49:26 +0500

    Profile Page Completed

--- profile-page/App.js
+++ profile-page/App.js
@@ -4,11 +4,34 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.avatar_container}>
-        <View>
-          <Image source={require("./assets/batman-avatar.png")} />
+        <View style={{ justifyContent: "center", alignItems: "center" }}>
+          <Image
+            source={require("./assets/batman-avatar.png")}
+            style={{ marginBottom: 10 }}
+          />
+          <Text style={{ maxWidth: 200 }}>
+            "It's not who I am underneath, but what I do that defines me"
+          </Text>
         </View>
       </View>
-      <View style={styles.details_container}></View>
+      <View style={styles.details_container}>
+        <View style={styles.item}>
+          <Text style={styles.label}>Name</Text>
+          <Text style={styles.data}>Batman</Text>
+        </View>
+        <View style={styles.item}>
+          <Text style={styles.label}>City</Text>
+          <Text style={styles.data}>Gotham</Text>
+        </View>
+        <View style={styles.item}>
+          <Text style={styles.label}>Profession</Text>
+          <Text style={styles.data}>Police</Text>
+        </View>
+        <View style={styles.item}>
+          <Text style={styles.label}>Skills</Text>
+          <Text style={styles.data}>Killer</Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
@@ -20,9 +43,29 @@ const styles = StyleSheet.create({
   },
   avatar_container: {
     flex: 0.4,
+    justifyContent: "center",
+    alignItems: "center",
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
   },
   details_container: {
     backgroundColor: "#fff",
     flex: 0.6,
+  },
+  item: {
+    flex: 0.25,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    flexDirection: "row",
+    paddingHorizontal: 20,
+    alignItems: "center",
+  },
+  label: {
+    fontSize: 32,
+    flex: 0.9,
+    color: "orange",
+  },
+  data: {
+    fontSize: 32,
   },
 });
