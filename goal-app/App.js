919b974a3c7a9818b71b56c5ca620d76cc06da01
unknown <asadullahk15@gmail.com>
2022-05-10 23:20:08 +0500

    Working with Core Components

--- goal-app/App.js
+++ goal-app/App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Goal App</Text>
+      </View>
+      <Text>Hello World!</Text>
+      <Button title="Go to Goals" onPress={() => console.log("Go to Goals")} />
     </View>
   );
 }
@@ -13,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
