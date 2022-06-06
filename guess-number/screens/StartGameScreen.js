144a6d1c024f7224aa76ce65478847c434e37e82
Asadullah Khan <asadullahk15@gmail.com>
2022-06-06 21:03:58 +0500

    Guess Number Project

---
+++ guess-number/screens/StartGameScreen.js
@@ -0,0 +1,10 @@
+import { View, Text, TextInput } from "react-native";
+import React from "react";
+
+export default function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+    </View>
+  );
+}
