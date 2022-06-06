6b12ca41130905ea67a603caf465622a0bba18b7
Asadullah Khan <asadullahk15@gmail.com>
2022-06-06 21:43:03 +0500

    Configure Number Input

--- guess-number/App.js
+++ guess-number/App.js
@@ -2,11 +2,7 @@ import { StyleSheet, View } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  return (
-    <View style={styles.center}>
-      <StartGameScreen />
-    </View>
-  );
+  return <StartGameScreen />;
 }
 
 const styles = StyleSheet.create({
--- guess-number/screens/StartGameScreen.js
+++ guess-number/screens/StartGameScreen.js
@@ -1,12 +1,46 @@
-import { View, TextInput } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 export default function StartGameScreen() {
   return (
-    <View>
-      <TextInput />
+    <View style={styles.center}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardAppearance="dark"
+        keyboardType="numeric"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Start</PrimaryButton>
       <PrimaryButton>Reset</PrimaryButton>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  center: {
+    padding: 16,
+    marginTop: 100,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    // Android shadow
+    elevation: 8,
+    marginHorizontal: 24,
+    // IOS shadow
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    textAlign: "center",
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 1,
+    color: "#ddb52f",
+    marginVertical: 16,
+    fontWeight: "bold",
+  },
+});
