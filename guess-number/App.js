96845944ef56a617b09d44e6560dfff589752c2d
unknown <asadullahk15@gmail.com>
2022-06-12 23:08:58 +0500

    Add Gradient Color

--- guess-number/App.js
+++ guess-number/App.js
@@ -1,17 +1,17 @@
 import { StyleSheet, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <View style={styles.center}>
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.center}>
       <StartGameScreen />
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   center: {
     flex: 1,
-    color: "#cccc",
   },
 });
