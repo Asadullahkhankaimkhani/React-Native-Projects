6392cd3042349fb9a97221c7fc3aabcb28821a39
unknown <asadullahk15@gmail.com>
2022-06-13 23:29:22 +0500

    Added Background Color

--- guess-number/App.js
+++ guess-number/App.js
@@ -1,17 +1,28 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
     <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.center}>
-      <StartGameScreen />
+      <ImageBackground
+        source={require("./assets/image/background.png")}
+        resizeMode="cover"
+        style={styles.center}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   center: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
     flex: 1,
   },
 });
